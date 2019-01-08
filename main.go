69d2903347bde9507f0667f78484c80fde05ab3e
Ken Bailey <kgbailey92@gmail.com>
2019-01-08 11:27:53 -0600

    testing new fetches

--- main.go
+++ main.go
@@ -26,10 +26,26 @@ func main() {
 	}
 
 	// get users by space di
-	users, err := client.GetSpaceByID(spaces[0].ID)
+	// users, err := client.GetSpaceByID(spaces[0].ID)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(users.Name)
+
+	// test get custom reports by spaceid
+	// reports, err := client.GetCustomReportsBySpaceID(spaces[0].ID)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(reports.TeamReports[0].ID)
+
+	// get tickets by report space id -- FINISH
+	tickets, err := client.GetTicketsBySpaceAndReport(0, spaces[0].ID)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(users.Name)
 
+	for _, t := range tickets {
+		fmt.Println(t.Summary)
+	}
 }
