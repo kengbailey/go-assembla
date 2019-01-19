4fdb064023f90cf77b3544adabc7dc95b97701b5
Ken Bailey <kgbailey92@gmail.com>
2019-01-18 18:03:50 -0600

    updated space struct with more json labels

--- spaces.go
+++ spaces.go
@@ -15,36 +15,36 @@ const (
 
 // Space is the return object of all Assembla Space endpoints.
 type Space struct {
-	Status             int       `json:"status"`
-	BannerHeight       int       `json:"banner_height"`
-	Banner             string    `json:"banner"`
-	UpdatedAt          time.Time `json:"updated_at"`
-	LastPayerChangedAt time.Time `json:"last_payer_changed_at"`
-	TeamTabRole        int       `json:"team_tab_role"`
-	CreatedAt          time.Time `json:"created_at"`
-	Approved           bool      `json:"approved"`
-	TabsOrder          string    `json:"tabs_order"`
-	IsCommercial       bool      `json:"is_commercial"`
-	IsManager          bool      `json:"is_manager"`
-	TeamPermissions    int       `json:"team_permissions"`
-	CanJoin            bool      `json:"can_join"`
-	BannerText         string    `json:"banner_text"`
-	Restricted         bool      `json:"restricted"`
-	SharePermissions   bool      `json:"share_permissions"`
-	CanApply           bool      `json:"can_apply"`
-	IsVolunteer        bool      `json:"is_volunteer"`
-	PublicPermissions  int       `json:"public_permissions"`
-	WikiName           string    `json:"wiki_name"`
-	Name               string    `json:"name"`
-	Style              string    `json:"style"`
-	ParentID           string    `json:"parent_id"`
-	DefaultShowpage    string    `json:"default_showpage"`
-	Description        string    `json:"description"`
-	ID                 string    `json:"id"`
-	BannerLink         string    `json:"banner_link"`
-	CommercialFrom     time.Time `json:"commercial_from"`
-	RestrictedDate     string    `json:"restricted_date"`
-	WatcherPermissions int       `json:"watcher_permissions"`
+	Status             int       `json:"status,omitempty"`
+	BannerHeight       int       `json:"banner_height,omitempty"`
+	Banner             string    `json:"banner,omitempty"`
+	UpdatedAt          time.Time `json:"updated_at,omitempty"`
+	LastPayerChangedAt time.Time `json:"last_payer_changed_at,omitempty"`
+	TeamTabRole        int       `json:"team_tab_role,omitempty"`
+	CreatedAt          time.Time `json:"created_at,omitempty"`
+	Approved           bool      `json:"approved,omitempty"`
+	TabsOrder          string    `json:"tabs_order,omitempty"`
+	IsCommercial       bool      `json:"is_commercial,omitempty"`
+	IsManager          bool      `json:"is_manager,omitempty"`
+	TeamPermissions    int       `json:"team_permissions,omitempty"`
+	CanJoin            bool      `json:"can_join,omitempty"`
+	BannerText         string    `json:"banner_text,omitempty"`
+	Restricted         bool      `json:"restricted,omitempty"`
+	SharePermissions   bool      `json:"share_permissions,omitempty"`
+	CanApply           bool      `json:"can_apply,omitempty"`
+	IsVolunteer        bool      `json:"is_volunteer,omitempty"`
+	PublicPermissions  int       `json:"public_permissions,omitempty"`
+	WikiName           string    `json:"wiki_name,omitempty"`
+	Name               string    `json:"name,omitempty"`
+	Style              string    `json:"style,omitempty"`
+	ParentID           string    `json:"parent_id,omitempty"`
+	DefaultShowpage    string    `json:"default_showpage,omitempty"`
+	Description        string    `json:"description,omitempty"`
+	ID                 string    `json:"id,omitempty"`
+	BannerLink         string    `json:"banner_link,omitempty"`
+	CommercialFrom     time.Time `json:"commercial_from,omitempty"`
+	RestrictedDate     string    `json:"restricted_date,omitempty"`
+	WatcherPermissions int       `json:"watcher_permissions,omitempty"`
 }
 
 // GetUserSpaces returns a slice of spaces the specified user belongs to.
