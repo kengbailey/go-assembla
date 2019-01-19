f5b8e7a271897bbfa763819c04d38d156641f2ae
Ken Bailey <kgbailey92@gmail.com>
2019-01-18 18:05:52 -0600

    new testing

--- main.go
+++ main.go
@@ -21,27 +21,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// get users by space di
-	// users, err := client.GetSpaceByID(spaces[0].ID)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(users.Name)
-
-	// test get custom reports by spaceid
-	// reports, err := client.GetCustomReportsBySpaceID(spaces[0].ID)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(reports.TeamReports[0].ID)
-
-	// get tickets by report space id -- FINISH
-	tickets, err := client.GetTicketsBySpaceAndReport(0, spaces[0].ID)
-	if err != nil {
-		log.Fatal(err)
+	for _, space := range spaces {
+		fmt.Println(space.Name)
+		// get ACtive/followed tickets
+		tickets, err := client.GetFollowedTicketsBySpace(space.ID)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(len(tickets))
+		for _, ticket := range tickets {
+			fmt.Println(ticket.Summary)
+		}
+		fmt.Println()
 	}
-	fmt.Println(len(tickets))
-	// for _, t := range tickets {
-	// 	fmt.Println(t.Summary)
-	// }
 }
