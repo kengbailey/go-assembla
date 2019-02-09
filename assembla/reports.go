0d12fded8c09ef098c99a8032de59d4eaafa1f48
Ken Bailey <kgbailey92@gmail.com>
2019-02-09 12:29:22 -0600

    testing new json decoding

--- assembla/reports.go
+++ assembla/reports.go
@@ -2,6 +2,7 @@ package assembla
 
 import (
 	"encoding/json"
+	"net/http"
 	"strings"
 )
 
@@ -23,15 +24,50 @@ type ReportItem struct {
 }
 
 // GetCustomReportsBySpaceID ...
-func (s *ReportsService) GetCustomReportsBySpaceID(spaceID string) (reports Reports, err error) {
+func (s *ReportsService) GetCustomReportsBySpaceID(spaceID string) (Reports, error) {
 	url := strings.Replace(customReportURL, "spaceID", spaceID, -1)
-	body, err := s.client.FetchRequestBody(url)
+	var reports Reports
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return
+		return reports, err
 	}
-	err = json.Unmarshal(body, &reports)
+	req.Header.Set("X-Api-Key", s.client.key)
+	req.Header.Set("X-Api-Secret", s.client.secret)
+
+	resp, err := s.client.httpClient.Do(req)
 	if err != nil {
-		return
+		return reports, err
 	}
-	return
+	defer resp.Body.Close()
+
+	// var body []byte
+	// if resp.StatusCode == 200 {
+	// 	body, err = ioutil.ReadAll(resp.Body)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// } else if resp.StatusCode == 204 {
+	// 	return nil, errors.New("Failed Fetch! --> 204 No Content")
+	// } else if resp.StatusCode == 404 {
+	// 	return nil, errors.New("Failed Fetch! --> 404 Not Found")
+	// }
+	// return body, nil
+
+	err = json.NewDecoder(resp.Body).Decode(reports)
+	if err != nil {
+		return reports, err
+	}
+
+	// body, err := s.client.FetchRequestBody(url)
+	// if err != nil {
+	// 	return reports, err
+	// }
+
+	// err = json.Unmarshal(body, &reports)
+	// if err != nil {
+	// 	return reports, err
+	// }
+
+	return reports, nil
 }
